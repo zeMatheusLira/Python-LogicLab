defebf82933afa588f24313bb16ae99bec14738a
Zé Matheus <josemw86@gmail.com>
2024-10-03 12:54:21 -0300

    Adição aula01

---
+++ aulas/Aula01.py
@@ -0,0 +1,75 @@
+nome = input('Digite o seu nome:')
+print('Muito prazer {}!'.format(nome))
+
+
+#soma de dois números
+numero1 = int(input('Digite o número 1: '))
+numero2 = int(input('Digite o número 2: '))
+resultado = numero1 + numero2
+print('A soma dos dois número é igual: {}'.format(resultado))
+
+
+#antecessor e sucessor
+numero = int(input('Digite um número: '))
+a = numero - 1
+b = numero + 1
+print('O número {} tem como antecessor {} e tem como sucessor {}'.format(numero, a, b))
+
+
+#dobro, triplo e raiz
+# vamos usar a variavel numero
+dobro = numero*2
+triplo = numero*3
+raiz = (numero**(1/2))
+print('O número {} tem como dobro {} tem como triplo {} e como raiz quadrada {:.2f}'.format(numero, dobro, triplo, raiz))
+
+
+#resolver aula - média aritmética
+#
+
+
+#resolver aula - Escreva um programa que pergunte a quantidade de KM percorridos
+#por um carro alugado e a quantidade de dias pelo quais ele foi alugado.
+#calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por KM rodado.
+#
+#
+
+
+#for
+n = int(input('Digite um número: '))
+for i in range(0, n):
+    print(f"Os números até o {n} são: {i}")
+
+
+#while
+n2 = int(input('Digite um número: '))
+while n2 >=0:
+    print(n2)
+    n2 -=1
+
+
+#Apresente os números pares de um determinado valor - resolver aula
+n3 = int(input('Digite um número: '))
+for i in range(1, n3):
+    if i %2 == 0 :
+        print(i)
+
+
+#Crie um programa em python que adivinha um número de 1 a 10 - resolver aula
+import random
+numero_adivinhar = random.randint(1,10)
+
+
+numero_escolhido = 0
+
+
+while numero_escolhido != numero_adivinhar:
+    numero_escolhido = int(input('Adivinhe um número entre 1 e 10: '))
+    if numero_escolhido > numero_adivinhar:
+        print("Muito alto! Tente novamente.")
+    elif numero_escolhido  < numero_adivinhar:
+        print("Muito baixo! Tente novamente.")
+    else:
+        print("Parabéns! Você acertou!!.")
+
+        
